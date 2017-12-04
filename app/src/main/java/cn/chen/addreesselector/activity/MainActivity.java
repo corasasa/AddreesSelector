c191ad79da2917cc392549af416c5cc7a4e33431
chenjinglan <13925024285@163.com>
2017-12-04 14:44:18 +0800

    创建

---
+++ app/src/main/java/cn/chen/addreesselector/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.chen.addreesselector.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.chen.addreesselector.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
